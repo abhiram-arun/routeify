a8b014e7164b2eac0547ae62b8301822fcf721cf
abhiram-arun <abhiramarun2004@gmail.com>
2024-05-04 23:23:34 +0530

    logo

--- lib/Auth/views/login.dart
+++ lib/Auth/views/login.dart
@@ -12,78 +12,72 @@ class Login extends StatelessWidget {
     final _screenwidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
                 padding: const EdgeInsets.only(bottom: 0.8),
-                child: Text('kili',style: TextStyle(fontSize: 45,fontWeight: FontWeight.bold, color:Color.fromARGB(255, 179, 8, 8),),
+                child: Container(
+                  height: 80,
+                  child: Image.asset('assets/images/banner.png'),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: Icon(Icons.person_2),
+                  fillColor: Color.fromARGB(255, 255, 248, 248),
+
+                  // focusColor: AdvColors.loginfield,
+                  // hoverColor: AdvColors.loginfield,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(35),
+                      borderSide: BorderSide(width: 0.1)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(35),
+                      borderSide: BorderSide(width: 0.5)),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  hintText: "Username",
                 ),
               ),
-              SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                       decoration: InputDecoration(
-                              filled: true,
-                              prefixIcon: Icon(Icons.person_2),
-                              fillColor: Color.fromARGB(255, 255, 248, 248),
-                
-                              // focusColor: AdvColors.loginfield,
-                              // hoverColor: AdvColors.loginfield,
-                              enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(35),
-                                  borderSide: BorderSide(width: 0.1)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(35),
-                                  borderSide: BorderSide(width: 0.5)),
-                              disabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(35),
-                              ),
-                              hintText: "Username",
-                       ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: Icon(Icons.key),
+                  fillColor: Color.fromARGB(255, 255, 248, 248),
+
+                  // focusColor: AdvColors.loginfield,
+                  // hoverColor: AdvColors.loginfield,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(35),
+                      borderSide: BorderSide(width: 0.1)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(35),
+                      borderSide: BorderSide(width: 0.5)),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  hintText: "Password",
                 ),
               ),
-              SizedBox(height: 10.0,),
-                Padding(
-                  padding: const EdgeInsets.all( 8.0),
-                  child: TextField(
-                              decoration: InputDecoration(
-                                filled: true,
-                                prefixIcon: Icon(Icons.key),
-                                fillColor: Color.fromARGB(255, 255, 248, 248),
-                    
-                                // focusColor: AdvColors.loginfield,
-                                // hoverColor: AdvColors.loginfield,
-                                enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(35),
-                                    borderSide: BorderSide(width: 0.1)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(35),
-                                    borderSide: BorderSide(width: 0.5)),
-                                disabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(35),
-                                ),
-                                hintText: "Password",
-                              ),
-                                  ),
-                ),
-                SizedBox(height: 10.0),
-                ElevatedButton(
-                  child: Text('LOGIN'),
-                  onPressed: (){
-                     Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Userhome(),
-                    ),
-                  );
-                  }), 
-              TextButton(
-                        child: Text("User",),
-                        onPressed: ()
-                          {
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+                child: Text('LOGIN'),
+                onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -91,20 +85,31 @@ class Login extends StatelessWidget {
                     ),
                   );
                 }),
-              TextButton(
-                        child: Text("Admin"),
-                        onPressed: () {
-                           Navigator.push(
+            TextButton(
+                child: Text(
+                  "User",
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Userhome(),
+                    ),
+                  );
+                }),
+            TextButton(
+                child: Text("Admin"),
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => AdminHome(),
                     ),
                   );
-                        }
-              ),
-            ],
-          ),
+                }),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
