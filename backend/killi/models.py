82975ab1fffb58db5ca105cda24c277c0d1ff5ac
Yoshua Immanuel Raju <yoshuaraju@gmail.com>
2024-05-04 14:16:43 +0530

    New database

---
+++ backend/killi/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+# Create your models here.
+
+class Users(models.Model):
+    ID = models.AutoField(primary_key=True)
+    LOCATIONS = models.CharField(max_length=200)
+    QRDECODE = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.ID
+    
+class Buses(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    start = models.TimeField()
+    end = models.TimeField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.ID
+   
+class AVK(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Stop = models.CharField(max_length=100)
+    Time = models.TimeField()
+    Crowd = models.IntegerField(validators=[MaxValueValidator(100)])
+    def __str__(self):
+        return self.ID
+    
+class SHABARI(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Stop = models.CharField(max_length=100)
+    Time = models.TimeField()
+    Crowd = models.IntegerField(validators=[MaxValueValidator(100)])
+    def __str__(self):
+        return self.ID
+    
+class COMOS(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Stop = models.CharField(max_length=100)
+    Time = models.TimeField()
+    Crowd = models.IntegerField(validators=[MaxValueValidator(100)])
+    def __str__(self):
+        return self.ID
