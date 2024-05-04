42df15fa0fd0b91724d64cd7b9ae9013f0ceda8c
abhiram-arun <abhiramarun2004@gmail.com>
2024-05-05 04:38:01 +0530

    busfind

---
+++ lib/user/views/busfindview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BusFind extends StatelessWidget {
+  const BusFind({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
